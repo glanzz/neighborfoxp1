5c490b1b589291b0c385b768a4bcbb5fdc61adb2
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-03-03 13:31:42 -0500

    Setup User interface

---
+++ interface/foxp1/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import TableRow from './TableRow';
+
+var usage_data = {'VARIANT': 'c.532C>T p.(Gln178*)', 'ATTRIBUTES': {'il2': {'DNA BINDING_EFFECT': 21, 'CHANGING_POSITIONS': [], 'ALIGNMENT_SCORE': 3780.0, 'PERCENTAGE_IDENTITY': 71.91780821917808, 'M_GC': 41.48061104582844, 'C_GC': 31.963470319634702, 'M_HETEROZYGOSITY': 0.0, 'C_HETEROZYGOSITY': 0.0, 'TRANSITION_TRAVERSION_RATIO': 0.47280178837555886, 'GENETIC_DISTANCE': 0, 'NUCLEOTIDE_DIVERSITY': 0.7520928462709284, 'MUTATION_TYPE': 'Insertion'},'gatad2b': {'DNA BINDING_EFFECT': 21, 'CHANGING_POSITIONS': [], 'ALIGNMENT_SCORE': 6808.0, 'PERCENTAGE_IDENTITY': 9.694553221787112, 'M_GC': 41.48061104582844, 'C_GC': 40.88430046279815, 'M_HETEROZYGOSITY': 0.0, 'C_HETEROZYGOSITY': 0.0, 'TRANSITION_TRAVERSION_RATIO': 0.5050058892815077, 'GENETIC_DISTANCE': 0.1573098120181651, 'NUCLEOTIDE_DIVERSITY': 0.07278034887860449, 'MUTATION_TYPE': 'Deletion'}, 'foxp1': {'DNA BINDING_EFFECT': 21, 'CHANGING_POSITIONS': [], 'ALIGNMENT_SCORE': 0, 'PERCENTAGE_IDENTITY': 0, 'M_GC': 41.48061104582844, 'C_GC': 41.1085208121779, 'M_HETEROZYGOSITY': 0.0, 'C_HETEROZYGOSITY': 0.0, 'TRANSITION_TRAVERSION_RATIO': 0.4947776783049836, 'GENETIC_DISTANCE': 0.016048093593948935, 'NUCLEOTIDE_DIVERSITY': 0.007960004513158878, 'MUTATION_TYPE': 'Deletion'}, 'foxp4': {'DNA BINDING_EFFECT': 21, 'CHANGING_POSITIONS': [], 'ALIGNMENT_SCORE': 6808.0, 'PERCENTAGE_IDENTITY': 12.156274551817727, 'M_GC': 41.48061104582844, 'C_GC': 54.7157345903864, 'M_HETEROZYGOSITY': 0.0, 'C_HETEROZYGOSITY': 0.0, 'TRANSITION_TRAVERSION_RATIO': 0.5137395459976105, 'GENETIC_DISTANCE': 0.1996554105796209, 'NUCLEOTIDE_DIVERSITY': 0.09049353617598743, 'MUTATION_TYPE': 'Deletion'}, 'csf1r': {'DNA BINDING_EFFECT': 21, 'CHANGING_POSITIONS': [], 'ALIGNMENT_SCORE': 6808.0, 'PERCENTAGE_IDENTITY': 11.333255647483812, 'M_GC': 41.48061104582844, 'C_GC': 48.09808393401142, 'M_HETEROZYGOSITY': 0.0, 'C_HETEROZYGOSITY': 0.0, 'TRANSITION_TRAVERSION_RATIO': 0.511710643344204, 'GENETIC_DISTANCE': 0.1860474218130227, 'NUCLEOTIDE_DIVERSITY': 0.08488288858184481, 'MUTATION_TYPE': 'Deletion'}}, 'OBSERVATIONS': {'Severe': 'Mild'}}
+
+function App() {
+  const handleButtonOnClick = () => {
+    const input = document.getElementById("search").value;
+    // Send input to App
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <img className="logo" src="/logo.webp"/>
+        <input id="search" placeholder="Enter Variant" type="search" minLength="3" />
+        <button type="submit" onClick={handleButtonOnClick}>Submit</button>
+      </div>
+      <div className="content">
+          <div>
+            <div className="highlight">{usage_data["VARIANT"]}</div>
+            <TableRow data={usage_data["ATTRIBUTES"]} />
+          </div>
+          <div className='flex'>
+            <div>
+              <h2>Related Conditions</h2>  
+              <div>
+                Severity: {usage_data['OBSERVATIONS']['Severe']}
+              </div>
+            </div>
+            <div>
+              <h2>Action Items</h2>
+              <div>
+                <ul>
+                  <li>
+                  Assess need for:
+                    Community or online resources such as Parent to Parent;
+                    Social work involvement for parental support;
+                    Home nursing referral;
+                    Early intervention referral;
+                    Case management support referral.
+                  </li>
+                  <li>
+                  To inform affected persons & their families re nature, MOI, & implications of FOXP1 syndrome to facilitate medical & personal decision making
+                  </li>
+                  <li>
+                    To screen for behavior concerns incl ADHD, impulsivity, anxiety, sleep disturbances, &/or findings suggestive of ASD 1
+                  </li>
+                  <li>
+                  Evaluate speech production & receptive/expressive language in all persons, regardless of age.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ interface/foxp1/src/TableRow.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+const TableRow = ({data: Data}) => {
+  const indexingData = {
+    // BINDING: [key, key2]
+  };
+  function roundUpTextValue(text) {
+    // Convert the text to a number.
+    const number = parseFloat(text);
+  
+    // If the number is not a number, return the original text.
+    if (isNaN(number)) {
+      return text;
+    }
+  
+    // Round the number up to the nearest integer.
+    const roundedNumber = number.toFixed(2);
+  
+    // Convert the rounded number back to a string.
+    const roundedText = roundedNumber.toString();
+  
+    // Return the rounded text.
+    return roundedText;
+  }
+  const processData = (data) => {
+    Object.keys(data).forEach(key => {
+      Object.keys(data[key]).forEach(attribute => {
+        if (indexingData[attribute]) {
+          indexingData[attribute].push(data[key][attribute]);
+        } else {
+          indexingData[attribute] = [data[key][attribute]];
+        }
+      });
+    });
+    Object.keys(indexingData).forEach(indexDataKey => {
+      indexingData[indexDataKey].sort();
+    });
+    console.log(indexingData);
+  };
+
+  const getIndex = (key, value) => {
+    return indexingData[key] ? 
+      "index" + 
+      indexingData[key].findIndex((a) => a === value): 
+      '';
+  }
+
+  processData(Data);
+
+  console.log(Data);
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Neighbours</th>
+          <th>DNA Affinity</th>
+          {/*<th>Change Positions</th>*/}
+          <th>Alignment Score</th>
+          <th>Percentage Identity</th>
+          <th>GC Content (Comparing - Mutation)</th>
+          <th>Transition Traversion Ratio</th>
+          <th>Genetic Distance</th>
+          <th>Nucleotide Diversity</th>
+          <th>Probable mutation</th>
+        </tr>
+      </thead>
+        {console.log(Data)}
+      {Object.keys(Data).map(key => {
+        return (<tr>
+          <td>{key.toUpperCase()}</td>
+          <td className={getIndex("DNA BINDING_EFFECT", Data[key]["DNA BINDING_EFFECT"])}>{roundUpTextValue(Data[key]["DNA BINDING_EFFECT"])}</td>
+          {/*<td className={getIndex("CHANGING_POSITIONS", Data[key]["CHANGING_POSITIONS"])}>{Data[key]["CHANGING_POSITIONS"]}</td>*/}
+          <td className={getIndex("ALIGNMENT_SCORE", Data[key]["ALIGNMENT_SCORE"])}>{roundUpTextValue(Data[key]["ALIGNMENT_SCORE"])}</td>
+          <td className={getIndex("PERCENTAGE_IDENTITY", Data[key]["PERCENTAGE_IDENTITY"])}>{roundUpTextValue(Data[key]["PERCENTAGE_IDENTITY"])}</td>
+          <td className={getIndex("M_GC", Data[key]["M_GC"])}>{roundUpTextValue(Data[key]["M_GC"] - Data[key]["C_GC"])}</td>
+          <td className={getIndex("TRANSITION_TRAVERSION_RATIO", Data[key]["TRANSITION_TRAVERSION_RATIO"])}>{roundUpTextValue(Data[key]["TRANSITION_TRAVERSION_RATIO"])}</td>
+          <td className={getIndex("GENETIC_DISTANCE", Data[key]["GENETIC_DISTANCE"])}>{roundUpTextValue(Data[key]["GENETIC_DISTANCE"])}</td>
+          <td className={getIndex("NUCLEOTIDE_DIVERSITY", Data[key]["NUCLEOTIDE_DIVERSITY"])}>{roundUpTextValue(Data[key]["NUCLEOTIDE_DIVERSITY"])}</td>
+          <td className={getIndex("MUTATION_TYPE", Data[key]["MUTATION_TYPE"])}>{roundUpTextValue(Data[key]["MUTATION_TYPE"])}</td>
+        </tr>
+        );
+      })}
+    </table>
+  );
+};
+
+export default TableRow;
